894c88e303120e6f4abf3d775863be291331f9c6
satyam0601 <54521651+satyam0601@users.noreply.github.com>
2021-06-29 10:14:12 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-document.body.style.overflowX = "hidden";
-
 ReactDOM.render(<App />, document.getElementById("root"));
